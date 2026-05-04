1d13ccda3bc5c223b80ea457ca1420ee27175839
Joshua Payne <joshuabpayne87@gmail.com>
2026-05-03 21:11:56 -0500

    Add test endpoint to verify environment variables

---
+++ api/test-env.js
@@ -0,0 +1,13 @@
+// Test endpoint to check if environment variables are loaded
+export default async function handler(req, res) {
+  return res.status(200).json({
+    hasStripeKey: !!process.env.STRIPE_SECRET_KEY,
+    hasSupabaseKey: !!process.env.SUPABASE_SERVICE_ROLE_KEY,
+    hasLoopsKey: !!process.env.LOOPS_API_KEY,
+    hasWebhookSecret: !!process.env.STRIPE_WEBHOOK_SECRET,
+    hasSupabaseUrl: !!process.env.NEXT_PUBLIC_SUPABASE_URL,
+    hasAppUrl: !!process.env.NEXT_PUBLIC_APP_URL,
+    hasBaseUrl: !!process.env.NEXT_PUBLIC_BASE_URL,
+    nodeVersion: process.version
+  });
+}
